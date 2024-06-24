8cd74477877cf95f792dc3e1f765b47e1687d4e9
Brian Savage <savage13@gmail.com>
2024-06-24 07:45:32 -0400

    Upgrade button display

--- src/buttons.ts
+++ src/buttons.ts
@@ -20,62 +20,70 @@ function get_canvas(): any {
 const xscreen = get_canvas()
 const ctx = xscreen.getContext('2d');
 
-
-const d = 35
-const pos: { [key: string]: [number, number] } = {
-  A: [d, 0], B: [0, d], X: [0, -d], Y: [-d, 0],
-  Right: [d - 400, 0], Down: [-400, d], Up: [-400, -d], Left: [-d - 400, 0],
-  StickRRight: [d - 100, 100], StickRDown: [-100, d + 100], StickRUp: [-100, -d + 100], StickRLeft: [-d - 100, 100],
-  StickLRight: [d - 300, 100], StickLDown: [-300, d + 100], StickLUp: [-300, -d + 100], StickLLeft: [-d - 300, 100],
-  L: [-350, -100], R: [-50, -100], ZL: [-300, -130], ZR: [-100, -130], Minus: [-300, -40], Plus: [-100, -40],
+function circle(x: number, y: number, r: number, stroke: boolean, fill: boolean = false) {
+  ctx.beginPath()
+  ctx.arc(x, y, r, 0, 2 * Math.PI)
+  if (fill)
+    ctx.fill()
+  if (stroke)
+    ctx.stroke()
 }
-const trans: any = {
-  "Minus": "-", "Plus": "+",
-  "StickRLeft": "<", "StickRRight": ">", "StickRUp": "^", "StickRDown": "v",
-  "StickLLeft": "<", "StickLRight": ">", "StickLUp": "^", "StickLDown": "v",
-  "Left": "<", "Right": ">", "Up": "^", "Down": "v",
+
+const im = new Image()
+im.src = "./background.png"
+im.onload = () => {
+  draw_buttons(0)
+}
+
+const buttons: any = {
+  A: { x: 810, y: 250, r: 30, type: 'circle', stroke: true },
+  X: { x: 740, y: 178, r: 30, type: 'circle', stroke: true },
+  B: { x: 740, y: 320, r: 30, type: 'circle', stroke: true },
+  Y: { x: 670, y: 250, r: 30, type: 'circle', stroke: true },
+  StickL: { x: 198, y: 250, r: 120, type: 'circle', stroke: true },
+  StickR: { x: 1002, y: 250, r: 120, type: 'circle', stroke: true },
+  StickLUp: { x: 198, y: 250 - 60, r: 65, type: 'circle', stroke: false },
+  StickLDown: { x: 198, y: 250 + 60, r: 65, type: 'circle', stroke: false },
+  StickLLeft: { x: 198 - 60, y: 250, r: 65, type: 'circle', stroke: false },
+  StickLRight: { x: 198 + 60, y: 250, r: 65, type: 'circle', stroke: false },
+  StickRUp: { x: 1002, y: 250 - 60, r: 65, type: 'circle', stroke: false },
+  StickRDown: { x: 1002, y: 250 + 60, r: 65, type: 'circle', stroke: false },
+  StickRLeft: { x: 1002 - 60, y: 250, r: 65, type: 'circle', stroke: false },
+  StickRRight: { x: 1002 + 60, y: 250, r: 65, type: 'circle', stroke: false },
+  Minus: { x: 535, y: 78, r: 22, type: 'circle', stroke: true },
+  Plus: { x: 665, y: 78, r: 22, type: 'circle', stroke: true },
+  L: { x: 30, y: 15, w: 200, h: 80, r: 40, type: "rrect" },
+  R: { x: 970, y: 15, w: 200, h: 80, r: 40, type: "rrect" },
+  ZL: { x: 290, y: 15, w: 145, h: 80, r: 40, type: "rrect" },
+  ZR: { x: 765, y: 15, w: 145, h: 80, r: 40, type: "rrect" },
+  Up: { x: 442, y: 150, w: 55, h: 73, r: [40, 40, 0, 0], type: "rrect" },
+  Down: { x: 442, y: 150 + 73 + 55, w: 55, h: 73, r: [0, 0, 40, 40], type: "rrect" },
+  Left: { x: 442 - 73, y: 150 + 73, w: 73, h: 55, r: [40, 0, 0, 40], type: "rrect" },
+  Right: { x: 442 + 55, y: 150 + 73, w: 73, h: 55, r: [0, 40, 40, 0], type: "rrect" },
 }
 
 export function draw_buttons(button: HidNpadButton) {
-  const x0 = 1280 / 2
-  const y0 = 720 / 2
-  ctx.font = `22px ${FONT}`
-  ctx.textAlign = 'center';
-  ctx.textBaseline = 'middle';
-  ctx.fillStyle = "black"
-  for (const [key, xy] of Object.entries(pos)) {
-    ctx.save()
-    ctx.beginPath()
-    ctx.strokeStyle = "black"
-    ctx.lineWidth = 2
-    let x = x0 + xy[0]
-    let y = y0 + xy[1]
-    ctx.arc(x, y, 20, 0, 2.0 * Math.PI)
-    ctx.fillStyle = 'white'
-    ctx.fill()
-    ctx.stroke()
-    ctx.fillStyle = "black"
-    let k = trans[key] || key
-    ctx.fillText(k, x - 1, y + 1)
-    ctx.restore()
-  }
 
-  for (const key of Object.keys(pos)) {
-    // @ts-ignore
-    if (button & HidNpadButton[key]) {
-      if (key in pos) {
-        ctx.save()
-        ctx.beginPath()
-        ctx.fillStyle = "gray"
-        let x = x0 + pos[key][0]
-        let y = y0 + pos[key][1]
-        ctx.arc(x, y, 20, 0, 2.0 * Math.PI)
+  ctx.clearRect(0, 0, 1280, 720)
+  ctx.strokeStyle = "black"
+  ctx.fillStyle = "lightblue"
+  ctx.lineWidth = 8
+
+  for (const key of Object.keys(buttons)) {
+    const b = buttons[key]
+    if (b.type == "circle") {
+      // @ts-ignore
+      circle(b.x, b.y, b.r, b.stroke, button & HidNpadButton[key])
+    }
+    if (b.type == "rrect") {
+      ctx.beginPath()
+      ctx.roundRect(b.x, b.y, b.w, b.h, b.r)
+      // @ts-ignore
+      if (button & HidNpadButton[key]) {
         ctx.fill()
-        let k = trans[key] || key
-        ctx.fillStyle = "white"
-        ctx.fillText(k, x, y)
-        ctx.restore()
       }
+      ctx.stroke()
     }
   }
+
 }
